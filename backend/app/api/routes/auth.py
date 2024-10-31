708cbbe74f838262a6dda5ccbb24a460dd06ae90
su322 <3545799532@qq.com>
2024-10-31 19:30:44 +0800

    docs: 添加注释

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 auth_router = APIRouter()
 
-
+#auth模块实现 用户注册 登录 退出等功能
 @auth_router.get("/login")
 def login():
     return {"message": "Login"}
--- backend/app/api/routes/data.py
+++ backend/app/api/routes/data.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 data_router = APIRouter()
 
-
+#data模块实现 数据库读取数据 可视化展示 功能
 @data_router.get("/info")
 def get_data():
     return {"message": "Data Info"}
